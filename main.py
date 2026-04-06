9053ce55a51dd9fc30f02d6513b2a0adc816c092
183473562 <183473562@qq.com>
2026-04-06 11:33:36 +0800

    加一些股票注释！

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+# 访问 https://www.jetbrains.com/help/pycharm/ 这是一个百倍股票策略！！！！
+# 这是一个百倍股票策略！！！！
